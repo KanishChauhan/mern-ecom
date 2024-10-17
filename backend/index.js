f1bfe60bd5d42f486f16ce8ce04ae954041d3f0e
kanish chauhan <kanishchauhan959@gmail.com>
2024-10-17 23:40:45 +0530

    Database connected

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const product = require("./routes/productRoute");
+const connectDatabase = require("./database/db");
 
 const port = process.env.PORT;
+
+app.use(express.json());
+
+app.use("/api/v1", product);
+
+connectDatabase();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
