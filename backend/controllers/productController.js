72d71f43dd6faab6c3f1f668454fdec5850399c8
kanish chauhan <kanishchauhan959@gmail.com>
2024-10-22 23:41:54 +0530

    create and get products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,18 @@
-exports.getAllProducts = (req, res) => {
-  res.status(200).json({ message: "working fine" });
+const Product = require("../models/Product");
+
+//create product -- admin
+
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
+
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+
+  res.status(200).json({ message: "working fine", products });
 };
